ee255b6788c54a766ade7202275da620879e61c0
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-04-14 16:45:36 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in soundcloudproject.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/soundcloudproject.rb
@@ -0,0 +1,6 @@
+require "soundcloudproject/version"
+
+module Soundcloudproject
+  class Error < StandardError; end
+  # Your code goes here...
+end
